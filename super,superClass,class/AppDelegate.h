2d465971f298e4fb0c880ff07474f6e034ac7337
zhangyuhao <zhangyuhao1158@sina.com>
2016-07-12 17:51:36 +0800

    Initial Commit

---
+++ super,superClass,class/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  super,superClass,class
+//
+//  Created by zyh on 16/7/12.
+//  Copyright © 2016年 zyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ super,superClass,class/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  super,superClass,class
+//
+//  Created by zyh on 16/7/12.
+//  Copyright © 2016年 zyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
